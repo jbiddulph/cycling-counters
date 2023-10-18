92448e03fff9b3670b548898405bd7e598128878
jbiddulph <john.mbiddulph@gmail.com>
2023-10-18 21:33:49 +0100

    populating google maps with multiple markers

--- src/components/Counters/CounterItem.vue
+++ src/components/Counters/CounterItem.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <h2>Counter</h2>
-    {{ counter }}
+    <ul>
+      <li>Provider: <strong>{{ counter.provider }}</strong></li>
+      <li>Area: <strong>{{ counter.area }}</strong></li>
+      <li>SiteID: <strong>{{ counter.siteID }}</strong></li>
+      <li>Location: <strong>{{ counter.location }}</strong></li>
+      <li>Lat: <strong>{{ counter.latitude }}</strong></li>
+      <li>Long: <strong>{{ counter.longitude }}</strong></li>
+      <li>Start Time: <strong>{{ counter.startTime }}</strong></li>
+      <li>End Time: <strong>{{ counter.endTime }}</strong></li>
+      <li>Count: <strong>{{ counter.count }}</strong></li>
+      <li>Usmart ID: <strong>{{ counter.usmart_id }}</strong></li>
+    </ul>
   </div>
 </template>
 
--- src/components/Counters/CountersList.vue
+++ src/components/Counters/CountersList.vue
@@ -1,23 +1,52 @@
 <template>
   <div class="home container mx-auto">
-    <h2>Welcome</h2>
-    <div v-for="counter in counters" :key="counter.usmart_id">
-      <CounterItem :counter="counter" />
-    </div>
+    <GoogleMap
+      api-key="AIzaSyBWiPC71sMkSoaa0TNrioE8CP1Ll5HmpZ4"
+      style="width: 100%; height: 500px"
+      :center="center"
+      :zoom="12"
+    >
+      <!-- Use v-for to loop through counters and add markers -->
+      <Marker v-for="counter in counters" :options="markerOptions(counter)" :key="counter.usmart_id">
+        <!-- Use InfoWindow to display additional information -->
+        <InfoWindow :options="infoWindowOptions(counter)">
+          <CounterItem :counter="counter" />
+        </InfoWindow>
+      </Marker>
+    </GoogleMap>
   </div>
 </template>
 
 <script setup>
 import CounterItem from '@/components/Counters/CounterItem.vue'
 import { useCounterStore } from '@/stores/counterStore'
-import { onMounted, ref } from 'vue'
+import { GoogleMap, Marker, InfoWindow } from 'vue3-google-map'
+import { onMounted, ref, reactive } from 'vue'
 
 /* Counter */
 const counters = ref([])
+const center = reactive({
+  lat: 55.872011,
+  lng: -4.2658899
+})
 const counterStore = useCounterStore()
 
 onMounted(async () => {
     await counterStore.fetchCounters()
     counters.value = counterStore.counters
 })
+
+// Define marker options for each counter
+const markerOptions = (counter) => {
+  return {
+    position: { lat: counter.latitude, lng: counter.longitude }, // Set the marker position based on the counter's coordinate
+  }
+}
+
+// Define InfoWindow options for each counter
+const infoWindowOptions = (counter) => {
+  return {
+    position: { lat: counter.latitude, lng: counter.longitude }, // Set the InfoWindow position to match the marker
+  }
+}
 </script>
